c40cc415e09f386011cd588f4de69fcf4098d5a5
Juggernaut <108272701+ashish-aesthisia@users.noreply.github.com>
2024-04-01 18:47:33 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
+import subprocess
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
+subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
