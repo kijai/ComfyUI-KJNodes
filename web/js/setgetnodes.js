2f843db54e3e9553c9a2c801b96a377eb6195829
Kijai <40791699+kijai@users.noreply.github.com>
2024-05-16 15:04:33 +0300

    More Set/Get autocolors

--- web/js/setgetnodes.js
+++ web/js/setgetnodes.js
@@ -12,8 +12,13 @@ function setColorAndBgColor(type) {
         "IMAGE": LGraphCanvas.node_colors.pale_blue,
         "CLIP": LGraphCanvas.node_colors.yellow,
         "FLOAT": LGraphCanvas.node_colors.green,
-		"MASK": LGraphCanvas.node_colors.cyan,
+		"MASK": { color: "#1c5715", bgcolor: "#1f401b"},
 		"INT": { color: "#1b4669", bgcolor: "#29699c"},
+		"CONTROL_NET": { color: "#156653", bgcolor: "#1c453b"},
+		"NOISE": { color: "#2e2e2e", bgcolor: "#242121"},
+		"GUIDER": { color: "#3c7878", bgcolor: "#1c453b"},
+		"SAMPLER": { color: "#614a4a", bgcolor: "#3b2c2c"},
+		"SIGMAS": { color: "#485248", bgcolor: "#272e27"},
 
     };
 
