f7eb33abc80a2aded1b46dff0dd14d07856a7d50
kijai <40791699+kijai@users.noreply.github.com>
2025-06-18 11:03:51 +0300

    I remembered
    Should keep the id or node title methods viable, though not recommended

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -806,9 +806,10 @@ The choices are loaded from 'custom_dimensions.json' in the nodes folder.
        return (latent, int(width), int(height),)
 
 class WidgetToString:
-    # @classmethod
-    # def IS_CHANGED(cls, **kwargs):
-    #     return float("NaN")
+    @classmethod
+    def IS_CHANGED(cls, id, node_title, any_input, **kwargs):
+        if any_input is not None and (id != 0 or node_title != ""):
+            return float("NaN")
 
     @classmethod
     def INPUT_TYPES(cls):
