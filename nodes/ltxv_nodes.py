0e173bbfa9de9c070466609dc768219c6326b541
kijai <40791699+kijai@users.noreply.github.com>
2026-01-17 17:30:01 +0200

    LTX2_NAG: Better dtype cast

--- nodes/ltxv_nodes.py
+++ nodes/ltxv_nodes.py
@@ -388,9 +388,7 @@ class LTX2_NAG(io.ComfyNode):
 
         device = mm.get_torch_device()
         offload_device = mm.unet_offload_device()
-        dtype = model.model.diffusion_model.dtype
-        if dtype in [torch.float8_e4m3fn, torch.float8_e5m2]: # fallback for now
-            dtype = torch.bfloat16
+        dtype = model.model.manual_cast_dtype
 
         model_clone = model.clone()
 
